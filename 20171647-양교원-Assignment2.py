5b834e3cdc9f4cdeb67266addc1683fc5aef2974
Yang Kyowon <kyowonhorang@gmail.com>
2017-09-07 11:49:18 +0900

    Update 20171647-양교원-Assignment2.py

--- 20171647-양교원-Assignment2.py
+++ 20171647-양교원-Assignment2.py
@@ -6,8 +6,30 @@ def Factorial(inNumber):
     for x in range(2, inNumber+1):
         result *= x
     return result
+#end of Factorial func
 
-num = int(input(" Enter a number: " ) )
-while ( num > -1 ):
-    print( "%d!" %num, " = ", "%d" %Factorial(num) )
-    num = int(input(" Enter a number: " ) )
+def GetNumberInput():
+    num = 0
+    try:
+        num = int(input(" Enter a number: " ) )
+    except ValueError:
+        print("Wrong input value! <Casting Exception>")
+        return -1
+    finally:
+        return num
+#end of GetNumberInput func
+
+#main
+num = GetNumberInput()
+while ( num != -1 ):
+    if ( num == -1 ):
+        break
+    elif ( num < -1 ):
+        print("Wrong value")
+    else:
+        print( "%d!" %num, " = ", "%d" %Factorial(num) )
+
+    num = GetNumberInput()
+#end of while
+
+#end of main
