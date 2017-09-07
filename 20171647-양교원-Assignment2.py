9d5c5ad0cdc1d35abda24c943804e0a585e1cd08
Yang Kyowon <kyowonhorang@gmail.com>
2017-09-07 11:24:49 +0900

    Update 20171647-양교원-Assignment2.py

--- 20171647-양교원-Assignment2.py
+++ 20171647-양교원-Assignment2.py
@@ -1,8 +1,13 @@
 def Factorial(inNumber):
+    if ( inNumber <=  0 ):
+        return 1
+
     result = 1
     for x in range(2, inNumber+1):
         result *= x
     return result
 
-num = int(input(" Enter a number: " ))
-print( "%d!" %num, " = ", "%d" %Factorial(num) )
+num = int(input(" Enter a number: " ) )
+while ( num != -1 ):
+    print( "%d!" %num, " = ", "%d" %Factorial(num) )
+    num = int(input(" Enter a number: " ) )
