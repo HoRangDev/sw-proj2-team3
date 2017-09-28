75fbc89e378ed9354cb1e601e07b04ddfca14eb2
Yang Kyowon <kyowonhorang@gmail.com>
2017-09-28 11:44:56 +0900

    Added assignment 5

---
+++ 20171647_양교원_assignment5.py
@@ -0,0 +1,38 @@
+import time
+import random
+
+def iterfibo(n):
+    if ( n <= 1):
+        return n
+
+    
+    before = [1, 1]
+    current = 0
+    for count in range(n-2):
+        current = before[0] + before[1]
+        before[0] = before[1]
+        before[1] = current
+
+    return current
+
+def fibo(n):
+    if ( n <= 1 ):
+        return n
+
+    return fibo(n-1) + fibo(n-2)
+
+
+while True:
+	nbr = int(input("Enter a number: "))
+	if nbr == -1:
+		break
+	ts = time.time()
+	fibonumber = fibo(nbr)
+	ts = time.time() - ts
+	print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+
+
+	ts = time.time()
+	fibonumber = iterfibo(nbr)
+	ts = time.time() - ts
+	print("Iteration Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
