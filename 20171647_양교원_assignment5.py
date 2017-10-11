c633f0b0fd58c7473f7d43930bf7d65bfb68641c
Yang Kyowon <kyowonhorang@gmail.com>
2017-10-11 20:21:12 +0900

    Fixed when n is 2

--- 20171647_양교원_assignment5.py
+++ 20171647_양교원_assignment5.py
@@ -5,7 +5,7 @@ def iterfibo(n):
         return n
 
     before = [1, 1]
-    current = 0
+    current = 1
     for count in range(n-2):
         current = before[0] + before[1]
         before[0] = before[1]
@@ -13,20 +13,23 @@ def iterfibo(n):
 
     return current
 
-def fibo(n):
+def fibo(n, count):
+    count[0] += 1
     if (n <= 1):
         return n
 
-    return fibo(n-1) + fibo(n-2)
+    return fibo(n-1, count) + fibo(n-2, count)
 
 while True:
 	nbr = int(input("Enter a number: "))
 	if nbr == -1:
 		break
 	ts = time.time()
-	fibonumber = fibo(nbr)
+	count = [0]
+	fibonumber = fibo(nbr,count)
 	ts = time.time() - ts
 	print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+	print("Function recursively called: " + str(count[0]) + " times!")
 
 	ts = time.time()
 	fibonumber = iterfibo(nbr)
